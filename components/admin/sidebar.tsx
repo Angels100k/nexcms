0896a49d6c55e1abfd4de0cc20ea8dc95a12349a
Olvar <olvarjort@gmail.com>
2024-01-28 17:43:35 +0200

    update styling sidebar

--- components/admin/sidebar.tsx
+++ components/admin/sidebar.tsx
@@ -1,35 +1,45 @@
 import Link from 'next/link'
 import { SidebarProps } from  '@/interfaces/component/item'
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons'; // Import all solid icons
+
+library.add(fas);
+
 const Sidebar = () => {
     const { navs } = {
         navs: [
             {
+                icon: 'gauge',
                 text: 'Dashboard',
                 url: '/admin/dashboard'
             },
             {
+                icon: 'table-columns',
                 text: 'Pages',
                 url: '/admin/pages'
             },
             {
+                icon: 'globe',
                 text: 'Globals',
                 url: '/admin/globals'
             },
             {
+                icon: 'gear',
                 text: 'Settings',
                 url: '/admin/settings'
             }
         ]
     } as SidebarProps
     return (
-        <nav className="w-1/6 p-8 h-screen bg-gray-200 text-black">
-            <h1>Sidebar</h1>
+        <nav className="w-1/6 p-8 h-screen bg-primarybg text-sidetext">
+            <h1 className='pl-4 mb-4'>Sidebar</h1>
             <ul>
                 {navs.map((nav) => {
                     return (
                         <li key={nav.url}>
-                            <Link href={nav.url}>{nav.text}</Link>
+                            <Link className='py-2  hover:bg-gray-800 rounded-md flex' href={nav.url}>{nav.icon ? <FontAwesomeIcon height={20} className='mx-2' icon={['fas', nav.icon as any]} /> : ''}{nav.text}</Link>
                         </li>
                     )
                 })}
