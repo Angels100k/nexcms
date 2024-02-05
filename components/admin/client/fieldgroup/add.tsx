8c02bb7408db041405eb2918be4653120bf5d661
Olvar <olvarjort@gmail.com>
2024-02-05 18:04:48 +0100

    fix bug add fields can be empty

--- components/admin/client/fieldgroup/add.tsx
+++ components/admin/client/fieldgroup/add.tsx
@@ -6,13 +6,14 @@ const addGroup = (themeID: number) => {
     let sign = prompt("What is the name of the new group?");
     console.log(sign);
     sign = sign ? sign : "";
+    if(sign == "") return
     Insert(sign, themeID);
     // TODO: make it reload the element only not the whole page
     window.location.reload();
 }
 const GroupFields = ({themeID}: {themeID: number}) => {
     return (
-        <button onClick={e => addGroup(themeID)} >Add new group Item</button>
+        <button className="p-4 cursor-pointer hover:bg-slate-500 w-full" onClick={e => addGroup(themeID)} >Add new group Item</button>
     )
 }
 
